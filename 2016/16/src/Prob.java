3750de7353bfe119d8ac0d470bb6b13850707c67
fireduck64 <fireduck@gmail.com>
2021-12-26 21:22:07 -0800

    2016.16 incomplete

---
+++ 2016/16/src/Prob.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.util.*;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+public class Prob
+{
+
+  public static void main(String args[]) throws Exception
+  {
+    new Prob(new Scanner(new FileInputStream(args[0])));
+  }
+
+  Random rnd=new Random();
+
+  public Prob(Scanner scan)
+  {
+
+  }
+
+}
