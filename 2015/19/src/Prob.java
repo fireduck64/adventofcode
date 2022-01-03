74d79146eddb0f9af12b75b534d139d861aece4a
fireduck64 <fireduck@gmail.com>
2022-01-02 21:49:00 -0800

    Remove crap

--- 2015/19/src/Prob.java
+++ 2015/19/src/Prob.java
@@ -50,11 +50,7 @@ public class Prob
     }
 
     System.out.println("Part 1: " + transform(input_line).size());
-    System.out.println(rform);
 
-    Set<String> fing = ImmutableSet.of("e");
-    //System.out.println(getOptions("e","C",8));
-    //System.out.println(getOptions("e","Ti",8));
     SS fin = (SS) Search.searchPara(new SS(0, input_line));
     System.out.println(fin);
     System.out.println("Part 2: " + fin.getCost());
@@ -126,60 +122,6 @@ public class Prob
   }
 
 
-  HashMap<String, TreeMap<String, Integer>> memo = new HashMap<>();
-
-  // Maps result string to cost
-  public TreeMap<String, Integer> getOptions(String in, String req, int depth)
-  {
-    String key = in + "/" + req + "/" + depth;
-    synchronized(memo)
-    {
-      if (memo.containsKey(key)) return memo.get(key);
-    }
-
-
-    TreeMap<String, Integer> opt = new TreeMap<>();
-    if (in.equals(req))
-    {
-      opt.put(in, 0);
-    }
-    if (depth <= 0) return opt;
-    if (xform.containsKey(in))
-    {
-      for(String x : xform.get(in))
-      {
-        if (x.startsWith(req))
-        {
-          opt.put(x, 1);
-        }
-        else
-        {
-        String nx = readSymbol(x);
-        String rem_x = x.substring(nx.length());
-        for(Map.Entry<String, Integer> me : getOptions(nx, req, depth-1).entrySet())
-        {
-          String s = me.getKey() + rem_x;
-          int v = me.getValue();
-          if ((!opt.containsKey(s)) || (opt.get(s) > v))
-          {
-            opt.put(s, v+1);
-          }
-        }
-        }
-
-      }
-
-    }
-
-    synchronized(memo)
-    {
-      memo.put(key, opt);
-    }
-
-    return opt;
-  }
-
-
   
   public TreeSet<String> transform(String in)
   {
@@ -212,20 +154,6 @@ public class Prob
     return out;
 
   }
-
-  public String readSymbol(String in)
-  {
-    String s = "" + in.charAt(0);
-    if (in.length() > 1)
-    if (in.charAt(1) >= 'a')
-    if (in.charAt(1) <= 'z')
-    {
-      s+=in.charAt(1);
-    }
-    return s;
-
-  }
-
 
 
 }
